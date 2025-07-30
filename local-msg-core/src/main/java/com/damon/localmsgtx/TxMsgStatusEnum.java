96047560a65f76c31c7810ed1bfd1859cfec057a
damon <654894017@qq.com>
2025-07-31 00:04:12 +0800

    初始化工程

---
+++ local-msg-core/src/main/java/com/damon/localmsgtx/TxMsgStatusEnum.java
@@ -0,0 +1,23 @@
+package com.damon.localmsgtx;
+
+/**
+ * Transactional message status constants
+ */
+public enum TxMsgStatusEnum {
+
+    WAITING(1, "Waiting to send"),
+    SENT(2, "Sent");
+
+    private int status;
+
+    private String desc;
+
+    TxMsgStatusEnum(int status, String desc) {
+        this.status = status;
+        this.desc = desc;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
