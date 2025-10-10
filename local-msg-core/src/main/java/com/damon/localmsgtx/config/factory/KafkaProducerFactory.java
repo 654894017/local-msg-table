12d87df5f7c22b7bfe2db1779aab5843c6f1e888
Damon <654894017@qq.com>
2025-10-10 10:19:21 +0800

    Update KafkaProducerFactory.java

--- local-msg-core/src/main/java/com/damon/localmsgtx/config/factory/KafkaProducerFactory.java
+++ local-msg-core/src/main/java/com/damon/localmsgtx/config/factory/KafkaProducerFactory.java
@@ -15,6 +15,15 @@ public class KafkaProducerFactory {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.ACKS_CONFIG, "0");
         props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        // 批量发送相关配置
+        // 批次大小，单位字节，默认16384(16KB)
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 65536);
+        // 超过收集的时间的最大等待时长，单位：毫秒。
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+        // 启用压缩，可选gzip、snappy、lz4
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        // 缓冲区大小，默认33554432(32MB)
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 67108864);
         return new KafkaProducer<>(props);
     }
 
